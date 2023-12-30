d7062fa0fd8a313b12cd474eddf9e25b9d8198a7
hyedi3 <hyeri9918as@naver.com>
2023-12-31 06:38:41 +0900

    [fix] ageCalc.js (+ prompt())
    currentYear & birthYear 변수의 데이터를 default 값 대신 사용자 입력값으로 변경하였습니다.

--- ageCalculation/ageCalc.js
+++ ageCalculation/ageCalc.js
@@ -1,10 +1,10 @@
 // 버튼을 클릭했을 때, onclick에 설정한 calc 함수 실행 
 function calc() {
-    var currentYear = 2023;   // 올해 연도를 저장할 변수
-    var birthYear = 2000;     // 태어난 연도를 저장할 변수
-    var age;                 // 계산한 나이를 저장할 변수 
+    var currentYear = prompt("현재 년도를 입력하세요.", "YYYY"); // 사용자가 입력한 값을 저장하는 변수 (올해 년도)
+    var birthYear = prompt("태어난 년도를 입력하세요.", "YYYY");  // 사용자가 입력한 값을 저장하는 변수 (태어난 년도)
+    var age;   // 계산한 나이를 저장할 변수 
 
-    // 올해 연도에서 태어난 연도를 뺀 다음 1을 더하여 age에 저장
+    // 올해 년도에서 태어난 년도를 뺀 다음 1을 더하여 age에 저장
     age = currentYear - birthYear + 1; 
 
     // 문서(document)에서 선택자를 사용하여, (querySelector) id 값이 result인 태그("#result")를 선택하고 HTML에 삽입(innerHTML)
