c3f52e76f8d01746cdce5feaaa00e2a3ea189af3
hyedi3 <hyeri9918as@naver.com>
2024-02-16 22:58:53 +0900

    [comment] js 파일 주석 추가

--- threeMultiple/threeMultiple.js
+++ threeMultiple/threeMultiple.js
@@ -1,10 +1,11 @@
+// DOMContentLoaded 이벤트 :  HTML 문서의 모든 요소들이 로드되고 파싱되었을 때 발생하는 이벤트
 window.addEventListener('DOMContentLoaded', function () {
-    domReady();
+    domReady();                   // DOMContentLoaded 이벤트가 발생했을 때 domReady() 함수를 호출합니다.
  });
  
  function domReady() {
-     var displayArea = document.querySelector('.result');
-     var userNumber = prompt("숫자를 입력하세요.");
+     var displayArea = document.querySelector('.result');      // result 클래스를 찾아와 displayArea 변수에 저장합니다. 
+     var userNumber = prompt("숫자를 입력하세요.");        // 사용자로부터 숫자를 입력받습니다. 
  
      if (userNumber != null) {                          // [확인]을 눌렀을 때 실행
  
